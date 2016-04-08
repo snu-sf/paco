a4d56583184906675dfca6d8aa3db6d0ecd24433
U-gil-pc\gil <gil.hur@sf.snu.ac.kr>
2016-04-08 16:09:14 +0900

    Add the metasrc directory.

--- metasrc/build-distr.sh
+++ metasrc/build-distr.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-./make-tutorial.sh
+title="paco Tutorial"
+coqdoc -s --no-index --html paco*.v tutorial.v
+sed -i "s,<title>tutorial</title>,<title>$title</title>," tutorial.html
+
+mkdir -p paco paco/src paco/doc 
 chmod 644 *.css *.html *.v
 cp main.css coqdoc.css paco*.html tutorial.html paco/doc/
 cp hpattern.v paco*.v tutorial.v paco/src/
-
+cp README CHANGES LICENSE paco/
---
+++ metasrc/cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+make clean
+rm -f paco*.v *.pyc *.html
+rm -rf paco
